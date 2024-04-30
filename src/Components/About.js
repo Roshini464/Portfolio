44b3bc7faa2c01dd7586eee566b1e0ebcc1ea392
Roshini464 <143497920+Roshini464@users.noreply.github.com>
2024-04-30 23:56:45 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Skills from './Skills.js';
 import Tilt from 'react-parallax-tilt';
 import Lottie from "lottie-react"
 import Coder from '../LottieFiles/coder.json';
